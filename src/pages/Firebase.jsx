6d4539a5631318ec64d2911a78c71eb289d13750
binitkumar-0108 <binitkumar0108@gmail.com>
2026-06-27 11:39:53 +0530

    firebase setup

--- src/pages/Firebase.jsx
+++ src/pages/Firebase.jsx
@@ -7,17 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBhpAzbHblIkxQjIFrgDXzcLFEgQyCx580",
-  authDomain: "auth-01-08-2005.firebaseapp.com",
-  projectId: "auth-01-08-2005",
-  storageBucket: "auth-01-08-2005.firebasestorage.app",
-  messagingSenderId: "821487568102",
-  appId: "1:821487568102:web:9853d51616cf7381ce6727",
-  measurementId: "G-V5QB6EEDYH"
+  apiKey: "AIzaSyA2l4fGYhW9DZ6JZ62UYG1UX-SRMkBZly0",
+  authDomain: "rizz-chat-de318.firebaseapp.com",
+  projectId: "rizz-chat-de318",
+  storageBucket: "rizz-chat-de318.firebasestorage.app",
+  messagingSenderId: "288780156641",
+  appId: "1:288780156641:web:5ad9213391f741880281a0",
+  measurementId: "G-X3BK1W503Y"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export default app;
